277817b1738e2a97b4ea0e8bc6983709eae8ebf9
Sebastian Prati <sebastian.prati@live.com>
2021-07-05 18:50:39 -0300

    cambio Seba

--- weather-app/src/components/SearchBox.js
+++ weather-app/src/components/SearchBox.js
@@ -8,7 +8,8 @@ const SearchBox = () => {
                 <h2 className="text-secondary">Search for a city:</h2>
                 <div className="input-group mb-3">
                     <input type="text" className="form-control" id="searchinput" 
-// @ts-ignore
+// @ts-ignore 
+// eslint-disable-next-line
                     type="search" aria-label="Search"/>
                     <div className ="input-group-append">
                     <button className ="btn btn-primary" id="searchbtn" type ="submit">
