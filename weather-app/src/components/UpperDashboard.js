b316286cbe6eff6c21350e4e9620c2ec6fd310d0
sg0891502 <Daniel.Lagomarsino@sabre.com>
2021-07-09 02:00:11 -0300

    Calculos prontos

--- weather-app/src/components/UpperDashboard.js
+++ weather-app/src/components/UpperDashboard.js
@@ -9,17 +9,22 @@ const temperaturas = {
 }
 
 const valoresTemp = {
-    promMax:15,
-    promMin:0,
-    tempMax:10,
-    tempMin:0,
-    ciudad: ""
+    promMax: 15,
+    promMin: 0,
+    tempMax: 10,
+    tempMin: 0,
+    ciudad: "",
+    maxDias: [],
+    minDias: []
+
 }
 
-const UpperDashboard = (state= state.forecast) => {
+const UpperDashboard = (state = state.forecast) => {
     let forecast = useSelector(state => state.forecast);
     valoresTemp.ciudad = useSelector(state => state.ciudad);
-    calculos (forecast.list);
+    const valoresFinales = calculos(forecast.list);
+
+    console.log('valoresFinales', valoresFinales);
 
     return (
         <div className="col-md-12 border">
@@ -49,27 +54,27 @@ const UpperDashboard = (state= state.forecast) => {
                 <div className="col border">
                     <i className="bi bi-cloudy bi-2x" /><br />
                     <label className="text-secondary">MON</label><br />
-                    <label className="text-secondary">TEMP MON : {valoresTemp.tempMin}</label>
+                    <label className="text-secondary">TEMP MON : {valoresFinales.maxDias[0]} </label>
                 </div>
                 <div className="col border">
                     <i className="bi bi-cloudy bi-2x" /><br />
                     <label className="text-secondary">TUE</label><br />
-                    <label className="text-secondary">TEMP TUE: </label>
+                    <label className="text-secondary">TEMP TUE: {valoresFinales.maxDias[1]} </label>
                 </div>
                 <div className="col border">
                     <i className="bi bi-cloudy bi-2x" /><br />
                     <label className="text-secondary">WED</label><br />
-                    <label className="text-secondary">TEMP WED: </label>
+                    <label className="text-secondary">TEMP WED: {valoresFinales.maxDias[2]} </label>
                 </div>
                 <div className="col border">
                     <i className="bi bi-cloudy bi-2x" /><br />
                     <label className="text-secondary">THU</label><br />
-                    <label className="text-secondary">TEMP THU: </label>
+                    <label className="text-secondary">TEMP THU: {valoresFinales.maxDias[3]} </label>
                 </div>
                 <div className="col border">
                     <i className="bi bi-cloudy bi-2x" /><br />
                     <label className="text-secondary">FRI</label><br />
-                    <label className="text-secondary">TEMP FRI: </label>
+                    <label className="text-secondary">TEMP FRI: {valoresFinales.maxDias[4]} </label>
                 </div>
             </div>
         </div>
@@ -78,9 +83,45 @@ const UpperDashboard = (state= state.forecast) => {
 }
 
 
-const calculos = (_forecast) => {
-    console.log("calculos", _forecast);
-    //console.log(Object.size(_forecast));
+const calculos = (forecast) => {
+
+    let i = 0;
+    console.log("calculos", forecast);
+    forecast.forEach(element => {
+        i++;
+        console.log('Dia:' + i)
+        console.log('maxima:', element.temp.max);
+        console.log('minima:', element.temp.min);
+
+        valoresTemp.maxDias.push(Number(element.temp.max))
+        valoresTemp.minDias.push(Number(element.temp.min))
+    });
+
+    valoresTemp.tempMax = Math.max.apply(Math, valoresTemp.maxDias);
+    valoresTemp.tempMin = Math.min.apply(Math, valoresTemp.minDias);
+
+    valoresTemp.promMax = promedios(valoresTemp.maxDias);
+    valoresTemp.promMin = promedios(valoresTemp.maxDias);
+
+    console.log('maxima general:', valoresTemp.tempMax);
+    console.log('minima general:', valoresTemp.tempMin);
+    console.log('promedio maxima :', valoresTemp.promMax);
+    console.log('promedio minima :', valoresTemp.promMin);
+
+    return valoresTemp
 }
+
+const promedios = (temps) => {
+    let promedio = 0;
+    let i = 0
+
+    temps.forEach(element => {
+        i++;
+        promedio = promedio + element;
+    });
+
+    return parseFloat(promedio / i).toFixed(2)
+}
+
 
 export default UpperDashboard
